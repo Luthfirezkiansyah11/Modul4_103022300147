ee7db05be1a6842911759929ead39b41a200e3f3
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 16:17:44 +0700

    menambahkan class KodeProduk

---
+++ KodeProduk.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul4_103022300147
+{
+    internal class KodeProduk
+    {
+        public enum produk {
+            Laptop,
+            Smartphone,
+            Tablet,
+            Headset,
+            Keyboard,
+            Mouse,
+            Printer,
+            Monitor,
+            Smartwatch,
+            Kamera,
+        }
+
+        public static string GetKodeProduk(produk produk)
+        {
+            string[] kodeProduk = new string[] { "E100", "E101", "E102", "E103", "E104",
+                "E105", "E106", "E107", "E108", "E109" };
+
+            return kodeProduk[(int)produk];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Modul4_103022300147;
+
+Console.WriteLine("Input Nama Produk: ");
+string inputProduk = Console.ReadLine();
+
+if (Enum.TryParse(inputProduk, out KodeProduk.produk harga))
+{
+    string kodeProduk = KodeProduk.GetKodeProduk(harga);
+    Console.WriteLine($"Kode Produk : {kodeProduk}");
+}
+else
+{
+    Console.WriteLine($"Harga {inputProduk} : Tidak ada!");
+}
+
+
+
+
