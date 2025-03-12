5b05f271df3062fb100cdf7c72c8d51ec8bc27df
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 17:01:49 +0700

    menambahkan class FanLaptop

---
+++ FanLaptop.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul4_103022300147
+{
+    internal class FanLaptop
+    {
+        private enum State {Quiet, Balanced, Performance, Turbo }
+        private State currentState;
+
+        public FanLaptop()
+        {
+            currentState = State.Quiet;
+            Console.WriteLine("Fan Quiet");
+        }
+
+        public void modeBalanced()
+        {
+            if (currentState == State.Quiet)
+            {
+                currentState = State.Balanced;
+                Console.WriteLine("Fan Quiet berubah menjadi Balanced");
+            }
+            else
+            {
+                Console.WriteLine("Sudah Mode Balanced");
+            }
+            
+        }
+
+        public void modePerformance()
+        {
+            if (currentState == State.Balanced)
+            {
+                currentState = State.Performance;
+                Console.WriteLine("Fan Balanced berubah menjadi Performance");
+            }
+            else
+            {
+                Console.WriteLine("Sudah Mode Performance");
+            }
+        }
+
+        public void modeTurbo()
+        {
+            if (currentState == State.Performance)
+            {
+                currentState = State.Turbo;
+                Console.WriteLine("Fan Performance berubah menjadi Turbo");
+            }
+            else if (currentState == State.Quiet)
+            {
+                currentState = State.Turbo;
+                Console.WriteLine("Fan Quiet berubah menjadi Turbo");
+            }
+            else {
+                Console.WriteLine("Sudah Mode Turbo");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,6 +14,37 @@ else
     Console.WriteLine($"Harga {inputProduk} : Tidak ada!");
 }
 
+Console.WriteLine("\n");
 
+FanLaptop mode = new FanLaptop();
 
+while (true) {
+    Console.WriteLine("Input Mode Fan (Quiet, Balanced, Performance, Turbo / Keluar): ");
+    string inputMode = Console.ReadLine();
+
+    if (inputMode == "Quiet")
+    {
+        Console.WriteLine("Sudah Mode Quiet");
+    }
+    else if (inputMode == "Balanced")
+    {
+        mode.modeBalanced();
+    }
+    else if (inputMode == "Performance")
+    {
+        mode.modePerformance();
+    }
+    else if (inputMode == "Turbo")
+    {
+        mode.modeTurbo();
+    }
+    else if (inputMode == "Exit")
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Mode Fan Tidak Ada!");
+    }
+}
 
